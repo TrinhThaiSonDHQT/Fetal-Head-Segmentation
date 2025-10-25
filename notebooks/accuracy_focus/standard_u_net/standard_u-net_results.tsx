0bb457ea51b84f830668e67c7e5b039affb564d0
Trinh Thai Son <ttsondhqt@gmail.com>
2025-10-25 17:11:46 +0700

    Modified Standard U-Net notebook to use Dice Loss instead of Binary Cross-Entropy.

--- notebooks/accuracy_focus/standard_u_net/standard_u-net_results.tsx
+++ notebooks/accuracy_focus/standard_u_net/standard_u-net_results.tsx
@@ -1,6 +1,6 @@
 Epoch 1/100
   Train Loss: 0.4920
-  Val Loss:   0.4327
+  Val Loss:   0.4283
   Val Dice:   0.0000
   Val IoU:    0.0000
   Val PA:     0.9908
@@ -8,35 +8,115 @@ Epoch 1/100
   → Saved best model (Dice: 0.0000)
 
 Epoch 2/100
-  Train Loss: 0.3626
-  Val Loss:   0.3514
+  Train Loss: 0.3629
+  Val Loss:   0.3524
   Val Dice:   0.0000
   Val IoU:    0.0000
-  Val PA:     0.9918
+  Val PA:     0.9917
   LR:         0.000100
   → Saved best model (Dice: 0.0000)
 
 Epoch 3/100
-  Train Loss: 0.3216
-  Val Loss:   0.3009
+  Train Loss: 0.3273
+  Val Loss:   0.3449
+  Val Dice:   0.0007
+  Val IoU:    0.0004
+  Val PA:     0.9892
+  LR:         0.000100
+  → Saved best model (Dice: 0.0007)
+
+Epoch 4/100
+  Train Loss: 0.3038
+  Val Loss:   0.3007
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9920
+  LR:         0.000100
+
+Epoch 5/100
+  Train Loss: 0.2813
+  Val Loss:   0.2754
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9921
+  LR:         0.000100
+
+Epoch 6/100
+  Train Loss: 0.2520
+  Val Loss:   0.2421
   Val Dice:   0.0000
   Val IoU:    0.0000
   Val PA:     0.9923
   LR:         0.000100
-  → Saved best model (Dice: 0.0000)
 
-Epoch 4/100
-  Train Loss: 0.2857
-  Val Loss:   0.2933
+Epoch 7/100
+  Train Loss: 0.2473
+  Val Loss:   0.2433
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9918
+  LR:         0.000100
+
+Epoch 8/100
+  Train Loss: 0.2185
+  Val Loss:   0.2139
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9923
+  LR:         0.000100
+
+Epoch 9/100
+  Train Loss: 0.2047
+  Val Loss:   0.2004
   Val Dice:   0.0000
   Val IoU:    0.0000
   Val PA:     0.9922
   LR:         0.000100
 
-Epoch 5/100
-  Train Loss: 0.2807
-  Val Loss:   0.2775
+Epoch 10/100
+  Train Loss: 0.1890
+  Val Loss:   0.1862
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9923
+  LR:         0.000100
+
+Epoch 11/100
+  Train Loss: 0.1773
+  Val Loss:   0.1781
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9923
+  LR:         0.000100
+
+Epoch 12/100
+  Train Loss: 0.1676
+  Val Loss:   0.1656
   Val Dice:   0.0000
   Val IoU:    0.0000
   Val PA:     0.9919
   LR:         0.000100
+
+Epoch 13/100
+  Train Loss: 0.1573
+  Val Loss:   0.1537
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9923
+  LR:         0.000100
+
+Epoch 14/100
+  Train Loss: 0.1470
+  Val Loss:   0.1443
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9923
+  LR:         0.000050
+
+Epoch 15/100
+  Train Loss: 0.1405
+  Val Loss:   0.1414
+  Val Dice:   0.0000
+  Val IoU:    0.0000
+  Val PA:     0.9923
+  LR:         0.000050
