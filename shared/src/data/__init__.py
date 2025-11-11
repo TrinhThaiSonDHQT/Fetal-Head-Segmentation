5389dcf8609f82778ae6b18d7f4aa2b4532bf383
Trinh Thai Son <ttsondhqt@gmail.com>
2025-11-11 08:56:31 +0700

    Update

--- shared/src/data/__init__.py
+++ shared/src/data/__init__.py
@@ -3,6 +3,7 @@ Data loading and preprocessing utilities.
 """
 
 from .dataset import HC18Dataset
+from .dataset_v2 import LargeScaleDataset
 
 # Export dataset classes
-__all__ = ['HC18Dataset']
+__all__ = ['HC18Dataset', 'LargeScaleDataset']
